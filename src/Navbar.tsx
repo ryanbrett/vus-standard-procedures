854548158b84bf5a50acdf106f7a6b741a66ad6f
Ryan McCullough <ryanbrett@users.noreply.github.com>
2025-12-10 15:15:24 -0600

    Fix link order in Navbar component

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -6,8 +6,8 @@ export default function Navbar() {
       {/* Note: In a Single Page App (React), we usually don't use .html extensions.
          We point to routes like '/info' or '/calculator'. 
       */}
-      <a href="/" className="mr-4">Info</a>
-      <a href="/calculator" className="ml-4 mr-4">Calculator</a>
+      <a href="/info" className="mr-4">Info</a>
+      <a href="/" className="ml-4 mr-4">Calculator</a>
       <a href="/resources" className="ml-4">Resources</a>
     </div>
   );
