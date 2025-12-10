0d2575a3203e06aea653ab47d91fc63663be2c2d
Ryan McCullough <ryanbrett@users.noreply.github.com>
2025-12-10 15:20:48 -0600

    Update Navbar links to use placeholders

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,12 +3,20 @@ export default function Navbar() {
   return (
     // In React, use 'className' instead of 'class'
     <div className="text-left p-4 text-white">
-      {/* Note: In a Single Page App (React), we usually don't use .html extensions.
-         We point to routes like '/info' or '/calculator'. 
-      */}
-      <a href="/info" className="mr-4">Info</a>
-      <a href="/" className="ml-4 mr-4">Calculator</a>
-      <a href="/resources" className="ml-4">Resources</a>
+      {/* Placeholder */}
+      <a href="#" onClick={(e) => e.preventDefault()} className="mr-4 hover:text-blue-400">
+        Info
+      </a>
+
+      {/* This link now points to your main home page (the Calculator) */}
+      <a href="/" className="ml-4 mr-4 font-bold text-blue-400 hover:text-blue-300">
+        Calculator
+      </a>
+
+      {/* Placeholder */}
+      <a href="#" onClick={(e) => e.preventDefault()} className="ml-4 mr-4 hover:text-blue-400">
+        Resources
+      </a>
     </div>
   );
 }
